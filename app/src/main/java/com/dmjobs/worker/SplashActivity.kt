3cbcb0a85aa7eeede3912cc663c47abeaa140fd2
TRADELUXEFX <tradeluxefx@gmail.com>
2026-09-06 10:36:10 +0100

    Create SplashActivity.kt

---
+++ app/src/main/java/com/dmjobs/worker/SplashActivity.kt
@@ -0,0 +1,25 @@
+package com.dmjobs.worker
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    private val splashDelayMs = 700L
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            if (!isFinishing) {
+                startActivity(Intent(this, LoginActivity::class.java))
+                finish()
+            }
+        }, splashDelayMs)
+    }
+}
+
